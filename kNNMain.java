566d484e4a058acc84f69a1958f21516c8b4dc52
sambhatnagar <sameer.bhatnagar@gmail.com>
2019-05-06 23:59:22 -0400

    update kNNMain.java to get add task where students must write the euclidean distance methods themselves

--- kNNMain.java
+++ kNNMain.java
@@ -20,12 +20,17 @@ public class kNNMain{
 
 
 
-    // TASK 4: Use the KNNClassifier class to determine the k nearest neighbors to a given DataPoint,
+    // TASK 4: write a new method in DataSet.java which takes as arguments to DataPoint objects,
+    // and returns the Euclidean distance between those two points (as a double)
+
+
+
+    // TASK 5: Use the KNNClassifier class to determine the k nearest neighbors to a given DataPoint,
     // and make a print a predicted target label
 
 
 
-    // TASK 5: loop over the datapoints in the held out test set, and make predictions for Each
+    // TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
     // point based on nearest neighbors in training set. Calculate accuracy of model.
 
 
